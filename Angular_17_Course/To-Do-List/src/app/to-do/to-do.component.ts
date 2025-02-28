afb3b451ad77d8f00d5a1b0fcdcd955c852bea48
aissa <mouhadjeraissa49@gmail.com>
2025-02-28 11:17:44 +0100

    add edit and remove in todo project

--- Angular_17_Course/To-Do-List/src/app/to-do/to-do.component.ts
+++ Angular_17_Course/To-Do-List/src/app/to-do/to-do.component.ts
@@ -1,9 +1,14 @@
 import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-to-do',
-  imports: [FontAwesomeModule],
+  imports: [
+    FontAwesomeModule,
+    FormsModule
+  ],
   templateUrl: './to-do.component.html',
   styleUrl: './to-do.component.css'
 })
@@ -13,10 +18,32 @@ export class ToDoComponent {
 
   tasks : string[] = [];
   newTask : string = "";
+  isAvailibale: boolean = false;
 
   addTask(){
-    if(this.newTask !== ""){
-      this.tasks.push(this.newTask)
+    if (this.newTask.trim()) {
+      this.isAvailibale = true;
+      this.tasks.push(this.newTask.trim());
+      this.newTask = "";
+    }
+  }
+
+  removeTask(idxTask: number) {
+    this.tasks.splice(idxTask, 1);
+    if (this.tasks.length < 1)
+      this.isAvailibale = false;
+  }
+
+  editTask(idxTask: number, newTask: string) : string | void{
+    if (idxTask >= 0 && idxTask < this.tasks.length){
+      let trimmedTask = newTask.trim();
+      
+      if (trimmedTask !== "") { // Prevent empty or unchanged value
+        if(trimmedTask !== this.tasks[idxTask])
+        this.tasks[idxTask] = trimmedTask;
+      }else{
+        alert("Put new Value in input of new task after that click in edit")
+      }
     }
   }
 }
